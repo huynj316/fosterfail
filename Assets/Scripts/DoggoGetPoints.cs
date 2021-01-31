746c210478498c227739fd2a2577169ce637b312
Julie Huynh <juliehuynh@Julies-MacBook-Pro.local>
2021-01-31 13:57:15 -0800

    add doggo and human pts

---
+++ Assets/Scripts/DoggoGetPoints.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class DoggoGetPoints : MonoBehaviour
+{
+    public ScoreController MyScoreController;
+    public Object CollisionParticles;
+    public GameObject DoggoController;
+    public GameObject HumanController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyScoreController = FindObjectOfType<ScoreController>();
+        CollisionParticles = Resources.Load("Prefab/ParticleSystem");
+        DoggoController = GameObject.Find("ThirdPersonDoggoController");
+        HumanController = GameObject.Find("ThirdPersonHumanController");
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (DoggoController != null)
+        {
+            if (collision.transform == DoggoController.transform)
+            {
+                ContactPoint contact = collision.contacts[0];
+                Quaternion rotation = Quaternion.FromToRotation(Vector3.up, contact.normal);
+                Vector3 position = contact.point;
+                Instantiate(CollisionParticles, position, rotation);
+                MyScoreController.DoggoScore++;
+
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/UI & Score/SceneScoreController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneScoreController : MonoBehaviour
+{
+    public List<GameObject> interactableDObjs = new List<GameObject>();
+    public List<GameObject> interactableHObjs = new List<GameObject>();
+    //private DoggoGetPoints doggoGetPoints; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        //doggo point setup
+        GameObject[] dInteractables = interactableDObjs.ToArray();
+
+        dInteractables = GameObject.FindGameObjectsWithTag("dog");
+
+        foreach (GameObject obj in dInteractables)
+        {
+            Debug.Log("obj" + obj.name);
+            obj.AddComponent<Rigidbody>();
+            obj.AddComponent<DoggoGetPoints>();
+
+        }
+
+        //human point setup
+        GameObject[] hInteractables = interactableHObjs.ToArray();
+
+        hInteractables = GameObject.FindGameObjectsWithTag("human");
+
+        foreach (GameObject obj in hInteractables)
+        {
+            Debug.Log("obj" + obj.name);
+            obj.AddComponent<Rigidbody>();
+            obj.AddComponent<HumanGetPoints>();
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
