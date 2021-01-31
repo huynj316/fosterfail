3babb52d77a97ab1da14dddc465df02a0b7fdd0c
jackieneon <simoj208@newschool.edu>
2021-01-31 12:34:29 -0500

    doors and grass

---
+++ Assets/Door Interaction Examples - FREE/Scripts/ButtonRaycastDoor/ButtonDoorController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonDoorController : MonoBehaviour
+{
+    [Header("Door Object")]
+    [SerializeField] private Animator doorAnim = null;
+
+    private bool doorOpen = false;
+
+    [Header("Door Animation Names")]
+    [SerializeField] private string openAnimationName = "DoorOpen";
+    [SerializeField] private string closeAnimationName = "DoorClose";
+
+    [Header("Pause Timer")]
+    [SerializeField] private int waitTimer = 1;
+    [SerializeField] private bool pauseInteraction = false;
+
+    private IEnumerator PauseDoorInteraction()
+    {
+        pauseInteraction = true;
+        yield return new WaitForSeconds(waitTimer);
+        pauseInteraction = false;
+
+    }
+
+    public void PlayAnimation()
+    {
+        if (!doorOpen && !pauseInteraction)
+        {
+            doorAnim.Play(openAnimationName, 0, 0.0f);
+            doorOpen = true;
+            StartCoroutine(PauseDoorInteraction());
+        }
+
+        else if (doorOpen && !pauseInteraction)
+        {
+            doorAnim.Play(closeAnimationName, 0, 0.0f);
+            doorOpen = false;
+            StartCoroutine(PauseDoorInteraction());
+        }
+    }
+}
---
+++ Assets/Door Interaction Examples - FREE/Scripts/KeyRaycastDoor/KeyItemController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyItemController : MonoBehaviour
+{
+    [Header("Object Type")]
+    [SerializeField] private bool redDoor = false;
+    [SerializeField] private bool redKey = false;
+
+    [Header("Inventory")]
+    [SerializeField] private KeyDoorInventory keyInventory = null;
+
+    private KeyDoorController doorObject;
+
+    private void Start()
+    {
+        doorObject = GetComponent<KeyDoorController>();
+    }
+
+    public void ObjectInteraction()
+    {
+        if (redDoor)
+        {
+            doorObject.PlayAnimation();
+        }
+
+        else if (redKey)
+        {
+            keyInventory.hasRedKey = true;
+            gameObject.SetActive(false);
+        }
+    }
+}
